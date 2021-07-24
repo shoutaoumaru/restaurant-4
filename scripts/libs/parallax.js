3cf0c9f34c2b3793a6231baf048ae05044bb4630
shoutaoumaru <otokmae.black@gmail.com>
2021-07-24 12:53:58 +0900

    レイアウトデバック

--- scripts/libs/parallax.js
+++ scripts/libs/parallax.js
@@ -1,27 +1,25 @@
-// //パララックス縦
-// const parallax = document.querySelectorAll('.parallax');
-// new simpleParallax(parallax, {
-//   scale: 1.3
-// });
-// //パララックス横右
-// const parallax01 = document.querySelectorAll('.parallax-rt');
-// new simpleParallax(parallax01, {
-//   orientation: 'right',
-//   scale: 1.3
-// });
-// const parallax02 = document.querySelectorAll('.parallax-lt');
-// new simpleParallax(parallax02, {
-//   orientation: 'left',
-//   scale: 1.3
-// });
-
-//* * *  * * *  パララックスbg* *  * * * * * * /
-
-var rellax = new Rellax('.rellax', {
-  speed: -2,
-  center: false,
-  wrapper: null,
-  round: true,
-  vertical: true,
-  horizontal: false
+//パララックス縦
+const parallax = document.querySelectorAll('.parallax-img');
+new simpleParallax(parallax, {
+  scale: 1.2,
+  delay: 0.4,
+  orientation: 'down'
+});
+const parallax03 = document.querySelectorAll('.parallax');
+new simpleParallax(parallax03, {
+  scale: 1.2,
+  delay: 0.4,
+  overflow: true,
+  orientation: 'down'
+});
+//パララックス横右
+const parallax01 = document.querySelectorAll('.parallax-rt');
+new simpleParallax(parallax01, {
+  orientation: 'right',
+  scale: 1.3
+});
+const parallax02 = document.querySelectorAll('.parallax-lt');
+new simpleParallax(parallax02, {
+  orientation: 'left',
+  scale: 1.3
 });
