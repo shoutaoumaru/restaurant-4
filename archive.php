7f011633d8b143b5d362d91b9b8f73b00d165973
aikawatakuya <85001312+aikawatakuya@users.noreply.github.com>
2021-09-26 16:38:15 +0900

    モバイルスクロール　画像変更php

--- archive.php
+++ archive.php
@@ -43,6 +43,10 @@
       <!-- /.sp-navbtn -->
     </header>
     <!-- /#header -->
+    <div class="c-top-view">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/home/cafe.jpg" alt="" />
+      </div>
+      <!-- /.c-top-view -->
     <section class="c-news">
       <div class="c-container">
         <h3 class="c-news__title">
